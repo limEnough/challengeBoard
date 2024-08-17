dddc303219aee769463658c59295a43a33f39f66
limEnough <limEnough@gmail.com>
2024-08-17 16:27:35 +0900

    fix: next config 파일 내 sass loader option 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
-import { fileURLToPath } from "url";  
-import path from "path";
+/**
+ * [참고문서] sassOptions
+ * https://velog.io/@juice-han/dirname-is-not-defined-in-ES-module-scope-%EC%98%A4%EB%A5%98%EC%9D%98-%EC%9B%90%EC%9D%B8%EA%B3%BC-%ED%95%B4%EA%B2%B0%EB%B0%A9%EB%B2%95
+ */
+import path from "path"; // 파일이나 디렉토리 경로를 다루는 path 모듈
+import { fileURLToPath } from 'url'; // 일반적인 파일 url을 Node.js 파일 path로 바꿔주는 함수
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const __filename = fileURLToPath(import.meta.url); // server.js 의 파일 url을 가져와서 fileURLToPath 로 경로 변환하여 저장
+const __dirname = path.dirname(__filename); // 변환된 경로의 디렉터리 경로를 __dirname 에 저장
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -11,10 +16,10 @@ const nextConfig = {
       missingSuspenseWithCSRBailout: false 설정을 통해 Suspense 컴포넌트의 CSR 대체를 허용하지 않음으로써 SSR에서 발생할 수 있는 문제를 명확히 파악할 수 있게 함
     */
     missingSuspenseWithCSRBailout: false,
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')], // sassOptions 옵션 추가
-      prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`, // prependData 옵션 추가
-    },
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`, // prependData 옵션 추가
   },
 };
 
