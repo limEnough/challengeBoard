5b8aaabb3be768ffa765170f8cd45df2ea664233
limEnough <limEnough@gmail.com>
2024-08-28 23:53:10 +0900

    feat: header에 현재 월 정보 추가

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,8 +2,13 @@ import Link from "next/link";
 import styles from "./header.module.scss";
 import React from "react";
 import Image from "next/image";
+import { getMonth, Month } from "date-fns";
+import { enUS } from "date-fns/locale";
 
 const Header: React.FC = () => {
+  const today = new Date();
+  const month = getMonth(today) as Month;
+
   return (
     <header className={styles.header}>
       <Link href='/' className={styles.logo}>
@@ -14,6 +19,7 @@ const Header: React.FC = () => {
           height={32}
           priority // 리소스의 사전 로드 트리거
         />
+        <span className={styles.month}>{month + 1}</span>
       </Link>
       
     </header>
