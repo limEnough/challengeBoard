93b2a55e093028b314e258469d47c6712cd452f4
limEnough <limEnough@gmail.com>
2024-08-30 00:15:16 +0900

    refact: 소스 정리

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,9 @@ import { fetchUserPushList } from "./api/github";
 import Loading from "./components/loading";
 import Empty from "./components/empty";
 import ErrorPage from "./components/error";
-import '../styles/globals.scss';
 import { PushListResponse } from "./api/github.types";
 import { getFilteredPushListByDate, makeBoardData } from "./utils/github";
+import '../styles/globals.scss';
 
 const noto = Noto_Sans_KR({
   subsets: ['latin'], // 또는 preload: false
@@ -26,34 +26,34 @@ export default function Home() {
   const [error, setError] = useState<boolean>(false);
 
   const [selectedDate, setSelectedDate] = useState<SelectedDate>(new Date());
+
   const [user1GithubPushList, setUser1GithubPushList] = useState<PushListResponse[] | null>(null);
   const [user2GithubPushList, setUser2GithubPushList] = useState<PushListResponse[] | null>(null);
-  const [userInfo, setUserInfo] = useState<Board[]>([]);
-  const [userInfo1, setUserInfo1] = useState<Board | null>(null);
-  const [userInfo2, setUserInfo2] = useState<Board | null>(null);
+
+  const [userData1, setUserData1] = useState<Board | null>(null);
+  const [userData2, setUserData2] = useState<Board | null>(null);
+
+  const [boardData, setBoardData] = useState<Board[]>([]);
   // #endregion
 
   // #region API
   /** 깃헙 푸시 내역 조회 */
   const getGithubPushList = useCallback(async () => {
-    const user1 = process.env.NEXT_PUBLIC_GITHUB_HER_USERNAME as string;
-    const user2 = process.env.NEXT_PUBLIC_GITHUB_HIM_USERNAME as string;
-
     try {
       setLoading(true);
       
-      const userInfo1 = await fetchUserPushList({
-        username: user1,
+      const userData1 = await fetchUserPushList({
+        username: process.env.NEXT_PUBLIC_GITHUB_HER_USERNAME as string,
         date: selectedDate,
       });
 
-      const userInfo2 = await fetchUserPushList({
-        username: user2,
+      const userData2 = await fetchUserPushList({
+        username: process.env.NEXT_PUBLIC_GITHUB_HIM_USERNAME as string,
         date: selectedDate,
       });
 
-      setUser1GithubPushList(userInfo1 ?? null);
-      setUser2GithubPushList(userInfo2 ?? null);
+      setUser1GithubPushList(userData1 ?? null);
+      setUser2GithubPushList(userData2 ?? null);
     } catch (error) {
       setError(true);
     } finally {
@@ -67,34 +67,49 @@ export default function Home() {
   const getCommitDataByDate = useCallback(async () => {
     if (!user1GithubPushList?.length || !user2GithubPushList?.length) return;
 
-    setUserInfo([]); //초기화
+    setLoading(true);
+    
+    setBoardData([]); //초기화
 
     const filteredUser1ByDate = getFilteredPushListByDate(user1GithubPushList, selectedDate);
     const filteredUser2ByDate = getFilteredPushListByDate(user2GithubPushList, selectedDate);
 
-    if (filteredUser1ByDate) setUserInfo1(makeBoardData(filteredUser1ByDate));
-    if (filteredUser2ByDate) setUserInfo2(makeBoardData(filteredUser2ByDate));
-  }, [selectedDate])
+    if (filteredUser1ByDate) setUserData1(makeBoardData(filteredUser1ByDate));
+    else setUserData1(null);
+
+    if (filteredUser2ByDate) setUserData2(makeBoardData(filteredUser2ByDate));
+    else setUserData2(null);
+
+    setLoading(false);
+  }, [selectedDate]);
+
+  /** 페이지 초기 실행 함수 (비동기 함수로 분리) */
+  const pageInit = useCallback(async () => {
+    await getGithubPushList();
+    getCommitDataByDate();
+  }, [])
   // #endregion
 
   // #region uesEffect
+  /** 날짜 업데이트 감지 */
   useEffect(() => {
     getCommitDataByDate();
   }, [selectedDate]);
 
+  /** 유저 별 데이터 업데이트 감지 */
   useEffect(() => {
     const result = [];
 
-    if (userInfo1) result.push(userInfo1);
-    if (userInfo2) result.push(userInfo2);
+    if (userData1) result.push(userData1);
+    if (userData2) result.push(userData2);
 
-    setUserInfo(result)
-  }, [userInfo1, userInfo2]);
+    setBoardData(result)
+  }, [userData1, userData2]);
   // #endregion
-
+  
   // 초기 실행
   useEffect(() => {
-    getGithubPushList();
+    pageInit();
   }, []);
 
   return (
@@ -118,13 +133,13 @@ export default function Home() {
           ?
           <Loading />
           :
-            !userInfo.length
+            !boardData.length
             ?
             <Empty />
             :
             <>
             {
-              userInfo && userInfo.map((info, index) => {
+              boardData && boardData.map((info, index) => {
                 return (
                   <Board
                     info={info}
