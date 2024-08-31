c1ed6c7a2791e838803f49e408634aa60b3a7585
limEnough <limEnough@gmail.com>
2024-09-01 02:38:41 +0900

    feat: body 바로 밑에 모달 위치할 수 있도록 포탈 생성

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
           <Header />
           {children}
         </div>
+
+        {/* Portal */}
+        <div id="root-modal"></div>
       </body>
     </html>
   );
