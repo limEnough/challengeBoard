7ae34a99abfea9fcc98693a52b24da2d80801906
limEnough <limEnough@gmail.com>
2024-09-01 02:39:49 +0900

    feat: 커밋 카운트 (이미지) 클릭 시, 커밋 목록 모달이 노출될 수 있도록 로직 수정

--- app/components/board/Board.tsx
+++ app/components/board/Board.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import styles from "./board.module.scss";
 import Image from 'next/image';
+import { useState } from 'react';
+import Modal from '../modal/Modal';
 
 interface Board {
   name: string;
@@ -14,56 +16,64 @@ interface BoardProps {
 }
 
 const Board = ({info}: BoardProps) => {
+  const [openDetails, setOpenDetails] = useState(false);
+
   return (
-    <div className={styles.board}>
-      {/* 깃헙 링크 */}
-      <Link href={info.url} className={styles.link}>
-        {/* 유저 이미지 */}
-        <Image
-          src={info.avatarUrl}
-          alt="User Avatar Image"
-          width={32}
-          height={32}
-          priority
-        />
-
-        {/* 유저 정보 */}
-        <div className={styles.userInfo}>
-          <strong>{info.name}</strong>
-        </div>
-      </Link>
-
-      {/* 깃헙 커밋 정보 */}
-      <div className={styles.grassInfo}>
-        <span>오늘의 수확</span>
-
-        {/* TODO: 상세 페이지 */}
-        <Link href='/' className={styles.link}>
+    <>
+      {/* Board */}
+      <div className={styles.board}>
+        {/* 깃헙 링크 */}
+        <Link href={info.url} className={styles.link}>
+          {/* 유저 이미지 */}
           <Image
-            src={info.pushCount > 0 ? '/images/grass--active.svg' : '/images/grass.svg'}
-            alt="User Image"
-            width={24}
-            height={34}
+            src={info.avatarUrl}
+            alt="User Avatar Image"
+            width={32}
+            height={32}
             priority
           />
-          <strong>{info.pushCount}</strong>
+
+          {/* 유저 정보 */}
+          <div className={styles.userInfo}>
+            <strong>{info.name}</strong>
+          </div>
         </Link>
+
+        {/* 깃헙 커밋 정보 */}
+        <div className={styles.grassInfo}>
+          <span>오늘의 수확</span>
+
+          {/* 커밋 리스트 모달 */}
+          <button type='button' onClick={() => setOpenDetails(true)}>
+            <Image
+              src={info.pushCount > 0 ? '/images/grass--active.svg' : '/images/grass.svg'}
+              alt="User Image"
+              width={24}
+              height={34}
+              priority
+            />
+            <strong>{info.pushCount}</strong>
+          </button>
+        </div>
+
+        {/* TODO: 듀오링고 출석 정보 */}
+        <div className={styles.duoInfo}>
+          <span>듀오 했나요?</span>
+
+          {/* info.attendedDuo */}
+          <Image
+            src={true ? '/images/alphabet--active.svg' : '/images/alphabet.svg'}
+            alt="User Image"
+            width={20}
+            height={30}
+            priority
+          />
+        </div>
       </div>
 
-      {/* TODO: 듀오링고 출석 정보 */}
-      <div className={styles.duoInfo}>
-        <span>듀오 했나요?</span>
-
-        {/* info.attendedDuo */}
-        <Image
-          src={true ? '/images/alphabet--active.svg' : '/images/alphabet.svg'}
-          alt="User Image"
-          width={20}
-          height={30}
-          priority
-        />
-      </div>
-    </div>
+      {/* Detail Modal */}
+      <Modal isOpen={openDetails} contents={info.commitMessages} />
+    </>
   );
 }
 
