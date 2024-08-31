c000936079e239aff26d12c60bcd1c8e9974042c
limEnough <limEnough@gmail.com>
2024-09-01 01:33:04 +0900

    refact: 미사용 소스 제거 및 header 내 현재 월 표시 삭제

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -2,23 +2,18 @@ import Link from "next/link";
 import styles from "./header.module.scss";
 import React from "react";
 import Image from "next/image";
-import { getMonth, Month } from "date-fns";
 
 const Header: React.FC = () => {
-  const today = new Date();
-  const month = getMonth(today) as Month;
-
   return (
     <header className={styles.header}>
       <Link href='/' className={styles.logo}>
         <Image
-          src="/logo.svg"
+          src="/images/logo.svg"
           alt="Challenge Board Logo"
           width={50}
           height={32}
           priority // 리소스의 사전 로드 트리거
         />
-        <span className={styles.month}>{month + 1}</span>
       </Link>
       
     </header>
