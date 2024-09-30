f5c4b4d8fbc803a40702bb48b8daa3db9a3c0e1a
limEnough <limEnough@gmail.com>
2024-09-30 23:39:45 +0900

    feat: header에 알림 설정 토글 버튼 추가

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,9 +1,26 @@
+"use client";
+
 import Link from "next/link";
-import styles from "./header.module.scss";
 import React from "react";
 import Image from "next/image";
+import { NextApiRequest, NextApiResponse } from "next";
+import { sendFCMNotification } from "@/app/api/notification";
+import styles from "./header.module.scss";
 
 const Header: React.FC = () => {
+  /** TODO: Toggle 푸시 알림 설정 */
+  const clickPushHandler = () => {
+    Notification.requestPermission().then(permission => {
+      if (permission !== 'granted') {
+        // 푸시 거부됐을 때 처리할 내용
+        console.log('푸시 거부됨');
+      } else {
+        // 푸시 승인됐을 때 처리할 내용
+        console.log('푸시 승인됨');
+      }
+    });
+  };
+
   return (
     <header className={styles.header}>
       <Link href='/' className={styles.logo}>
@@ -16,6 +33,13 @@ const Header: React.FC = () => {
         />
       </Link>
       
+      <button
+        type="button"
+        onClick={clickPushHandler}
+        className={styles.alarm}
+      >
+        푸시 알림 설정 토글 
+      </button>
     </header>
   )
 }
